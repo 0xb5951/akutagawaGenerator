b5b35e7fd3bd46011fd7d5856360ab16de1b08b1
odrum428 <oxkeita@gmail.com>
2019-03-01 00:08:07 +0900

    前処理にコメントを追加

--- preprocess.py
+++ preprocess.py
@@ -10,14 +10,15 @@ file = open(path, 'r', encoding='Shift_JIS')
 
 source_text = file.read()
 
-count = source_text.count('\n')
-print(count)
-
+# 本文前の注釈にタグを埋め込んで、そこを元に本文を抽出
 text_tagging_hi = re.sub(r'--+', 'タグを埋め込みます', source_text)
 text_remove_tag = text_tagging_hi.split('タグを埋め込みます')[-1]
+# 単語に振ってあるルビを削除
 text_without_rubi = re.sub(r'《.+?》','', text_remove_tag)
+# 本文中にある注釈や解説を削除
 text_without_com = re.sub(r'［.+?］', '', text_without_rubi)
+# 出版社や作成日などの情報を削除
 output = text_without_com.split('底本')[0]
-print(output)
+# print(output)
 
 file = open('test_preprocess.txt','a',encoding='utf-8').write(output)
