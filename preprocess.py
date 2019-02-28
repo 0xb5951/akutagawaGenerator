8c4416ab5ee9600acbcbf2b3e2664944f27e88a0
odrum428 <oxkeita@gmail.com>
2019-03-01 01:26:29 +0900

    全てのファイルに対して前処理を実行する処理を実装

--- preprocess.py
+++ preprocess.py
@@ -5,20 +5,22 @@ import sys
 import re
 import os
 
-path = './source/藪の中.txt'
-file = open(path, 'r', encoding='Shift_JIS')
+path = './source/'
 
-source_text = file.read()
+files = os.listdir(path)
 
-# 本文前の注釈にタグを埋め込んで、そこを元に本文を抽出
-text_tagging_hi = re.sub(r'--+', 'タグを埋め込みます', source_text)
-text_remove_tag = text_tagging_hi.split('タグを埋め込みます')[-1]
-# 単語に振ってあるルビを削除
-text_without_rubi = re.sub(r'《.+?》','', text_remove_tag)
-# 本文中にある注釈や解説を削除
-text_without_com = re.sub(r'［.+?］', '', text_without_rubi)
-# 出版社や作成日などの情報を削除
-output = text_without_com.split('底本')[0]
-# print(output)
-
-file = open('test_preprocess.txt','a',encoding='utf-8').write(output)
+for file in files:
+    file_path = path + file
+    print(file_path)
+    with open(file_path, 'r', encoding='Shift_JIS') as file:
+        source_text = file.read()
+        # 本文前の注釈にタグを埋め込んで、そこを元に本文を抽出
+        text_tagging_hi = re.sub(r'--+', 'タグを埋め込みます', source_text)
+        text_remove_tag = text_tagging_hi.split('タグを埋め込みます')[-1]
+        # 単語に振ってあるルビを削除
+        text_without_rubi = re.sub(r'《.+?》','', text_remove_tag)
+        # 本文中にある注釈や解説を削除
+        text_without_com = re.sub(r'［.+?］', '', text_without_rubi)
+        # 出版社や作成日などの情報を削除
+        output = text_without_com.split('底本')[0]
+        output_file = open('preprocess_done.txt','a',encoding='utf-8').write(output)
