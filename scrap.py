a4c17558088986c624bd0470c4daf0cd77b763c0
odrum428 <oxkeita@gmail.com>
2019-11-12 02:44:05 +0900

    リファクタ

--- scrap.py
+++ scrap.py
@@ -7,38 +7,28 @@ import time
 site_url = "https://aozorashoin.com"
 auther_url = "https://aozorashoin.com/author/879"
 
-r = requests.get(auther_url)
+#著者リストから書籍へのリンク一覧を抜き出す
+def get_book_list(auther_url):
+    r = requests.get(auther_url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    author_page_a_tag = soup.find('table', class_="table-hover").find_all('a', href=re.compile('../title/.*'))
 
-# htmlをBeautifulSoupで扱う
-soup = BeautifulSoup(r.text, "html.parser")
+    book_links = [] 
+    for a_tag in author_page_a_tag:
+        book_links.append(str(a_tag.get('href').replace('..', '')))
+    return book_links
 
-# table table-hover配下のaタグを配列に格納
-author_page_a_tag = soup.find('table', class_="table-hover").find_all('a', href=re.compile('../title/.*'))
-
+# 文書ページからテキストのみを抜き出す
 def book_detail(book_link):
     rb = requests.get(site_url + book_link)
     book = BeautifulSoup(rb.text, "html.parser")
-    print()
     title = book.h1.text
-    print(title)
     with open('./source/'+title+'.txt', mode='x') as f:
         f.write(str(book.select("#text")))
     time.sleep(5)
     return
 
-book_links = [] 
-for a_tag in author_page_a_tag:
-    book_links.append(str(a_tag.get('href').replace('..', '')))
-
-print(book_links)
-
-# 対象の文書リンクを配列の長さだけループ
-for book_link in book_links:
-    book_detail(book_link)
-    break
-
-
-# ページ中のH1タグがタイトル。これを元にファイルを生成
-# divのid='text'配下に本文が格納されている
-# 本文へのリンクは`title/X'となっている
-# これの処理はあとで考える
+if __name__ == "__main__":
+    book_links = get_book_list(auther_url)
+    for book_link in book_links:
+        book_detail(book_link)
